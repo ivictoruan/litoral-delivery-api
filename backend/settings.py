161dab59bfdf57ffc4209f75f2982449b32f101b
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-03-27 22:33:07 -0300

    FEAT: modelo de Organização/Categoria de Organização/Dono;

--- backend/settings.py
+++ backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "products.apps.ProductsConfig",
+    "organizations.apps.OrganizationsConfig",
 ]
 
 MIDDLEWARE = [
